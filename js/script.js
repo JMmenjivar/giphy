fd2307c9af0d868ebc562df5d0478b01b90a2777
jessica <s_jessica.menjivaraquino@ousd.org>
2019-03-22 00:39:58 +0000

    Your message here

--- js/script.js
+++ js/script.js
@@ -3,9 +3,24 @@
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 /* global $ */
 
-$("#search-button").click(function(){
-  
-  
-  
+$('button').click(function(){ 
+    var searchTerm = $('input').val();
+    
+    
+    var request_url = "https://api.giphy.com/v1/gifs/search?q="+ searchTerm + "g&ratin=pg&api_key=dc6zaTOxFJmzC";
+     
+    $.ajax({
+      url: request_url,
+      method: "GET",
+      success: function(response) {
+          
+           
+              
+          
+           var pic_url = response.data[0].images.original.url;
+           $('.text-center').append('<img src=' + pic_url + '>');
+      },
+    }); 
+    
 });
 
